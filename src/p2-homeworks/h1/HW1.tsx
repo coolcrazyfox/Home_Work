2d0abbbedb998d4e23f1d6fc28598efaa36903c9
Andrei Malets <coolviktori@yandex.by>
2021-12-01 21:49:43 +0300

    Add files via upload

---
+++ src/p2-homeworks/h1/HW1.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import Message from "./Message";
+
+
+
+const messageData = {
+    id: 1,
+    avatar: 'https://sun9-74.userapi.com/Ph-WiuOtF985il9AvN9JqiCWedmHtSGSSTXrSA/ltEB2Z2-YO4.jpg',
+    name: 'Some Name',
+    message: 'some text',
+    time: '22:00',
+}
+
+function HW1() {
+    return (
+        <div >
+            <hr/>
+            homeworks 1
+            {/*<Message name=" Your name" tasks={messageData}/>
+
+            should work (должно работать)*/}
+
+            <Message
+                id={messageData.id}
+                avatar={messageData.avatar}
+                name={messageData.name}
+                message={messageData.message}
+               time={messageData.time}
+            />
+
+            {/*<hr/>*/}
+
+            {/*для личного творчества, могу проверить*/}
+            {/*<AlternativeMessage/>*/}
+
+        </div>
+    )
+}
+
+export default HW1
---
+++ src/p2-homeworks/h1/Message.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import m from './Message.module.css'
+
+
+type messageData = {
+    id?: number,
+    avatar: string,
+    name: string,
+    message: string,
+    time: string
+}
+function Message(props: messageData) {
+    return (
+        <div>
+            <div className={m.reviews}>
+                <figure className={m.avatar}>
+                    <img className={m.imgAvatar} src={props.avatar}  />
+                    <figcaption className={m.nameText}>{props.name}</figcaption>
+
+                </figure>
+                <div className={m.comment} >
+                    <div className={m.commentBubble} >
+                        <h1 className={m.commentText}>{props.message}</h1>
+                        <h3 className={m.author}>{props.name}</h3>
+                        <h3 className={m.time}>{props.time}</h3>
+                    </div>
+                </div>
+            </div>
+            <div className={m.inputMessageContainer}>
+                <input type="text" className={m.inputField} />
+                <button className={m.submitBtn} >send</button>
+            </div>
+        </div>
+    )
+}
+
+
+export default Message
