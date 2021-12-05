2cd684d4e03871aeeef0153374dd19f5f62f7c2a
Andrei Malets <coolviktori@yandex.by>
2021-12-05 14:39:08 +0300

    Update Message.tsx

--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -9,17 +9,18 @@ type messageData = {
     message: string,
     time: string
 }
+
 function Message(props: messageData) {
     return (
         <div>
             <div className={m.reviews}>
                 <figure className={m.avatar}>
-                    <img className={m.imgAvatar} src={props.avatar}  />
+                    <img className={m.imgAvatar} src={props.avatar}/>
                     <figcaption className={m.nameText}>{props.name}</figcaption>
 
                 </figure>
-                <div className={m.comment} >
-                    <div className={m.commentBubble} >
+                <div className={m.comment}>
+                    <div className={m.commentBubble}>
                         <h1 className={m.commentText}>{props.message}</h1>
                         <h3 className={m.author}>{props.name}</h3>
                         <h3 className={m.time}>{props.time}</h3>
@@ -27,12 +28,10 @@ function Message(props: messageData) {
                 </div>
             </div>
             <div className={m.inputMessageContainer}>
-                <input type="text" className={m.inputField} />
-                <button className={m.submitBtn} >send</button>
+                <input type="text" className={m.inputField}/>
+                <button className={m.submitBtn}>send</button>
             </div>
         </div>
     )
 }
-
-
 export default Message
