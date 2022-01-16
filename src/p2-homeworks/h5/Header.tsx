7618a8730035b90ca57f35378ceed2c7ca691740
Andrei Malets <coolviktori@yandex.by>
2022-01-16 17:49:08 +0300

    Add files via upload

---
+++ src/p2-homeworks/h5/Header.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import s from './Header.module.css'
+import {PATH} from "./Routes";
+function Header() {
+    return (
+        <div className={s.header}>
+            <NavLink to={PATH.PRE_JUNIOR} className={s.link} activeClassName={s.active} >pre-junior</NavLink>
+            <NavLink to={PATH.JUNIOR} className={s.link} activeClassName={s.active} >junior</NavLink>
+            <NavLink to={PATH.JUNIOR_PLUS} className={s.link} activeClassName={s.active} >junior +</NavLink>
+
+
+            // add NavLinks
+            <div className={s.block}></div>
+
+
+        </div>
+    )
+}
+
+export default Header
---
+++ src/p2-homeworks/h5/pages/JuniorPlus.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+function JuniorPlus() {
+    return (
+        <div>
+            JuniorPlus
+        </div>
+    )
+}
+
+export default JuniorPlus
