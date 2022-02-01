445c709f5fc17069cf7ba1d9ab2bf7d9b445fb56
Andrei Malets <coolviktori@yandex.by>
2022-02-01 22:29:25 +0300

    Add files via upload

--- src/p1-main/m1-ui/u1-app/App.tsx
+++ src/p1-main/m1-ui/u1-app/App.tsx
@@ -5,6 +5,9 @@ import HW2 from "../../../p2-homeworks/h2/HW2";
 import HW3 from "../../../p2-homeworks/h3/HW3";
 import HW4 from "../../../p2-homeworks/h4/HW4";
 import HW5 from "../../../p2-homeworks/h5/HW5";
+import HW7 from "../../../p2-homeworks/h7/HW7";
+import HW8 from "../../../p2-homeworks/h8/HW8";
+import HW9 from "../../../p2-homeworks/h9/HW9";
 
 
 function App() {
@@ -15,7 +18,10 @@ function App() {
             {/*<HW2/>*/}
             {/*<HW3/>*/}
             {/*<HW4/>*/}
-            <HW5/>
+            {/*<HW5/>*/}
+            {/*<HW7/>*/}
+            {/*<HW8/>*/}
+            <HW9/>
         </div>
     )
 }
